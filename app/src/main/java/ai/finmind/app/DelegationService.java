48297159c37b695d50037b029e186ef24d6af0bb
patcharaph <patcharaph@gmail.com>
2026-01-05 01:52:57 +0700

    Add swipe action fade and TWA files

---
+++ app/src/main/java/ai/finmind/app/DelegationService.java
@@ -0,0 +1,14 @@
+package ai.finmind.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
